6c5855a0ccd3683a6ebf7d5ca97a8dab89fefd51
ALbertIM0427 <guang7226961@naver.com>
2024-09-24 15:53:26 +0900

    chore: APP 추가

--- weaverse/urls.py
+++ weaverse/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls")),
+    path("api/", include("jwtauth.urls")),
+    path("api/", include("courses.urls")),
+    path("api/", include("materials.urls")),
 ]
+s

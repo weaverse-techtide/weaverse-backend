2f6dbde2d32f4864bc454cc4580e935fab7e1418
yujeong <devnproyj22@gmail.com>
2024-09-25 08:07:06 +0900

    docs(changelog): write models of accounts, materials, courses and migrate

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('이메일은 필수입니다.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField('이메일 주소', unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- weaverse/urls.py
+++ weaverse/urls.py
@@ -9,3 +9,15 @@ urlpatterns = [
     path("api/", include("courses.urls")),
     path("api/", include("materials.urls")),
 ]
+
+
+# api_patterns = [
+#     path("accounts/", include("accounts.urls.api_urls")),
+#     path("accounts/", include("allauth.urls")),
+#     path("study/", include("study.urls.api_urls")),
+#     path("notifications/", include("notification.urls.api_urls")),
+# ]
+
+# urlpatterns = [
+#     path("api/v1/", include(api_patterns)),
+# ] 

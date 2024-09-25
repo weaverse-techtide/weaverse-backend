798b374705e5395b85d4a7f2a99146c36436a6b3
nathanLYJ <nathanlyj@gmail.com>
2024-09-25 10:24:27 +0900

    build: 블랙리스트 모델 추가

--- jwtauth/models.py
+++ jwtauth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BlacklistedToken(models.Model):
+    token = models.CharField(max_length=500)
+    blacklisted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Blacklisted token {self.token[:20]}... at {self.blacklisted_at}"

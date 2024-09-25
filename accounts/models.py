a574e2bf982ebdc84a704b263caee9c6b30b4c34
nathanLYJ <nathanlyj@gmail.com>
2024-09-25 10:21:16 +0900

    Refactor User model to use email as the username field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
+
+    def __str__(self):
+        return self.username

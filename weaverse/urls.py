a9ac7807b0210540707973bdebf01d6cd4cd77df
ALbertIM0427 <guang7226961@naver.com>
2024-09-26 15:08:08 +0900

    chore: swagger API 활성화

--- weaverse/urls.py
+++ weaverse/urls.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# Swagger 문서화를 위한 설정
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Weaverse API",
+        default_version="v1",
+        description="Weaverse 교육 플랫폼 API 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="Apache License 2.0"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,14 +26,15 @@ urlpatterns = [
     path("api/", include("materials.urls")),
 ]
 
-
-# api_patterns = [
-#     path("accounts/", include("accounts.urls.api_urls")),
-#     path("accounts/", include("allauth.urls")),
-#     path("study/", include("study.urls.api_urls")),
-#     path("notifications/", include("notification.urls.api_urls")),
-# ]
-
-# urlpatterns = [
-#     path("api/v1/", include(api_patterns)),
-# ] 
+# Swagger 문서화를 위한 URL 설정
+urlpatterns = [
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]

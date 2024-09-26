e383272bfb8e5bc178ab5832fb5409e1fe886606
nathanLYJ <nathanlyj@gmail.com>
2024-09-26 14:32:53 +0900

    refactor: Decrease token expiration times

--- jwtauth/utils.py
+++ jwtauth/utils.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 def generate_access_token(user):
     payload = {
         "user_id": user.id,
-        "exp": timezone.now() + timedelta(minutes=60),
+        "exp": timezone.now() + timedelta(minutes=5),
         "iat": timezone.now(),
     }
     return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
@@ -16,7 +16,7 @@ def generate_access_token(user):
 def generate_refresh_token(user):
     payload = {
         "user_id": user.id,
-        "exp": timezone.now() + timedelta(minutes=360),
+        "exp": timezone.now() + timedelta(minutes=60),
         "iat": timezone.now(),
     }
     return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")

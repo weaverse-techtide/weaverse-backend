a7a9cdd7f2dd3cbbaab3fb5276adf1516d36cdc5
nathanLYJ <nathanlyj@gmail.com>
2024-09-26 14:33:19 +0900

    Refactor: Remove unnecessary API paths in weaverse/urls.py

--- weaverse/urls.py
+++ weaverse/urls.py
@@ -13,10 +13,7 @@ urlpatterns = [
 =======
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("accounts.urls")),
     path("api/", include("jwtauth.urls")),
-    path("api/", include("courses.urls")),
-    path("api/", include("materials.urls")),
 ]
 
 urlpatterns += [

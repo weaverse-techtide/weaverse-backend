475e374ec6f18d0fb882fbf6ba9251cc2da0f7b4
yujeong <devnproyj22@gmail.com>
2024-09-28 20:34:45 +0900

    refactor: accounts의 url 수정 - reverse() 사용

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "accounts"
+
 urlpatterns = [
     path("users/", views.user_list_create, name="user-list-create"),
     path("users/<int:pk>/", views.user_retrieve_update_delete, name="user-detail"),

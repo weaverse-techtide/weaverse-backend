8769c3cd9ca874414c51c81c2237274b92c4ddc4
nathanLYJ <nathanlyj@gmail.com>
2024-09-30 13:22:20 +0900

    Refactor: access토큰에 권한정보 추가

--- jwtauth/utils.py
+++ jwtauth/utils.py
@@ -7,8 +7,9 @@ from django.utils import timezone
 def generate_access_token(user):
     payload = {
         "user_id": user.id,
+        "is_staff": user.is_staff,
+        "is_superuser": user.is_superuser,
         "exp": timezone.now() + timedelta(minutes=5),
-        "iat": timezone.now(),
     }
     return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
 
@@ -16,7 +17,6 @@ def generate_access_token(user):
 def generate_refresh_token(user):
     payload = {
         "user_id": user.id,
-        "exp": timezone.now() + timedelta(minutes=60),
-        "iat": timezone.now(),
+        "exp": timezone.now() + timedelta(minutes=120),
     }
     return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")

852b9f68f2868aaf29e1f4cc741cfba49d6d3990
Yoo117 <zerial117@gmail.com>
2024-09-30 10:29:44 +0900

    feat: 장바구니와 상품 모델 구현

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from courses.models import Curriculum, Course
+
+
+class Cart(models.Model):
+    """
+    유저의 장바구니 모델입니다.
+    """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.email}의 장바구니"
+
+
+class CartItem(models.Model):
+    """
+    장바구니에 담긴 상품 모델입니다.
+    """
+
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_items")
+    curriculum = models.OneToOneField(
+        Curriculum, on_delete=models.CASCADE, null=True, blank=True
+    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.cart.user.email}의 장바구니에 담긴 {self.curriculum.name}"

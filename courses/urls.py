0f370e07f6af72e141cb24aabf7b5e7d17de5e53
ALbertIM0427 <guang7226961@naver.com>
2024-10-01 12:42:22 +0900

    feat: curriculum 생성,수정,삭제, 목록 조회, 개별 조회 기능 구현

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import CourseDetailRetrieveUpdateDestroyView, CourseListCreateView
+from .views import (
+    CourseDetailRetrieveUpdateDestroyView,
+    CourseListCreateView,
+    CurriculumDetailRetrieveUpdateDestroyView,
+    CurriculumListCreateView,
+)
 
 app_name = "courses"
 urlpatterns = [
@@ -9,5 +14,15 @@ urlpatterns = [
         "courses/<int:pk>/",
         CourseDetailRetrieveUpdateDestroyView.as_view(),
         name="course-detail",
+    ),
+    path(
+        "curriculums/",
+        CurriculumListCreateView.as_view(),
+        name="curriculum-list",
+    ),
+    path(
+        "curriculums/<int:pk>",
+        CurriculumDetailRetrieveUpdateDestroyView.as_view(),
+        name="curriculum-detail",
     ),
 ]

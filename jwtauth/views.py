97090656b824077631f904e2076b1edc989d9ae2
nathanLYJ <nathanlyj@gmail.com>
2024-10-01 11:21:14 +0900

    Refactor: Add serializers for login, logout, and refresh token

--- jwtauth/views.py
+++ jwtauth/views.py
@@ -8,60 +8,79 @@ from .models import BlacklistedToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 import jwt
 from accounts.models import CustomUser
+from .serializers import LoginSerializer, LogoutSerializer, RefreshTokenSerializer
+from rest_framework.generics import GenericAPIView
 
 
-class LoginView(APIView):
+class LoginView(GenericAPIView):
     """
     사용자가 로그인할 때 사용, email과 password를 받아서 인증 합니다.
     email과 password가 일치하고 활성화된 사용자인 경우, access,refresh 토큰을 반환합니다.
     """
 
     permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
 
     def post(self, request):
-        email = request.data.get("email")
-        password = request.data.get("password")
-        user = authenticate(email=email, password=password)
 
-        if user is not None and user.is_active:
-            access_token = generate_access_token(user)
-            refresh_token = generate_refresh_token(user)
+        serializer = self.get_serializer(data=request.data)
 
-            return Response(
-                {"access_token": access_token, "refresh_token": refresh_token}
-            )
+        if serializer.is_valid():
+            email = serializer.validated_data["email"]
+            password = serializer.validated_data["password"]
 
+            user = authenticate(email=email, password=password)
+
+            if user is not None and user.is_active:
+                access_token = generate_access_token(user)
+                refresh_token = generate_refresh_token(user)
+
+                return Response(
+                    {"access_token": access_token, "refresh_token": refresh_token}
+                )
+
+            else:
+                return Response(
+                    {"error": "회원 가입하세요"}, status=status.HTTP_401_UNAUTHORIZED
+                )
         else:
-            return Response(
-                {"error": "회원 가입하세요"}, status=status.HTTP_401_UNAUTHORIZED
-            )
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-User = get_user_model()
-
-
-class LogoutView(APIView):
+class LogoutView(GenericAPIView):
     """
     사용자가 로그아웃할 때 사용, 현재 refresh 토큰을 블랙리스트에 추가합니다.
     """
 
+    permission_classes = [IsAuthenticated]
+    serializer_class = LogoutSerializer
+
     def post(self, request):
-        try:
-            refresh_token = request.data.get("refresh_token")
 
-            BlacklistedToken.objects.create(token=refresh_token, user=request.user)
-            return Response(
-                {
-                    "success": "로그아웃 완료. refresh 토큰이 블랙리스트에 추가되었습니다."
-                },
-                status=status.HTTP_200_OK,
-            )
+        serializer = self.get_serializer(data=request.data)
 
-        except Exception as e:
-            return Response(
-                {"error": f"블랙리스트에 추가하는 중 오류 발생: {str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+        if serializer.is_valid():
+            refresh_token = serializer.validated_data["refresh_token"]
+
+            try:
+                BlacklistedToken.objects.create(token=refresh_token, user=request.user)
+                return Response(
+                    {
+                        "success": "로그아웃 완료. refresh 토큰이 블랙리스트에 추가되었습니다."
+                    },
+                    status=status.HTTP_200_OK,
+                )
+
+            except Exception as e:
+                return Response(
+                    {"error": f"블랙리스트에 추가하는 중 오류 발생: {str(e)}"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+User = get_user_model()
 
 
 class RefreshTokenView(APIView):
@@ -71,49 +90,45 @@ class RefreshTokenView(APIView):
     """
 
     permission_classes = [AllowAny]
+    serializer_class = RefreshTokenSerializer
 
     def post(self, request):
-        refresh_token = request.data.get("refresh_token")
-        if not refresh_token:
-            return Response(
-                {"error": "리프레시 토큰이 제공되지 않았습니다."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        serializer = self.get_serializer(data=request.data)
 
-        try:
-            if BlacklistedToken.objects.filter(token=refresh_token).exists():
+        if serializer.is_valid():
+            refresh_token = serializer.validated_data["refresh_token"]
+
+            try:
+                payload = jwt.decode(
+                    refresh_token, settings.SECRET_KEY, algorithms=["HS256"]
+                )
+                user = CustomUser.objects.get(id=payload["user_id"])
+
+                access_token = generate_access_token(user)
+                new_refresh_token = generate_refresh_token(user)
+
+                BlacklistedToken.objects.create(
+                    token=refresh_token, user=user, token_type="refresh"
+                )
+                return Response(
+                    {"access_token": access_token, "refresh_token": new_refresh_token},
+                    status=status.HTTP_200_OK,
+                )
+
+            except jwt.ExpiredSignatureError:
+                return Response(
+                    {"error": "리프레시 토큰이 만료되었습니다."},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+            except (jwt.DecodeError, User.DoesNotExist):
                 return Response(
                     {"error": "유효하지 않은 리프레시 토큰입니다."},
                     status=status.HTTP_401_UNAUTHORIZED,
                 )
-
-            payload = jwt.decode(
-                refresh_token, settings.SECRET_KEY, algorithms=["HS256"]
-            )
-            user = CustomUser.objects.get(id=payload["user_id"])
-
-            access_token = generate_access_token(user)
-            refresh_token = generate_refresh_token(user)
-
-            BlacklistedToken.objects.create(
-                token=refresh_token, user=user, token_type="refresh"
-            )
-            return Response(
-                {"access_token": access_token, "refresh_token": refresh_token}
-            )
-
-        except jwt.ExpiredSignatureError:
-            return Response(
-                {"error": "리프레시 토큰이 만료되었습니다."},
-                status=status.HTTP_401_UNAUTHORIZED,
-            )
-        except (jwt.DecodeError, User.DoesNotExist):
-            return Response(
-                {"error": "유효하지 않은 리프레시 토큰입니다."},
-                status=status.HTTP_401_UNAUTHORIZED,
-            )
-        except Exception as e:
-            return Response(
-                {"error": f"토큰 갱신 중 오류 발생: {str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+            except Exception as e:
+                return Response(
+                    {"error": f"토큰 갱신 중 오류 발생: {str(e)}"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

7f3190046d27b9a3d5c1368fa75140e7d93f218a
nathanLYJ <nathanlyj@gmail.com>
2024-10-02 03:28:57 +0900

    Refactor: 주석 추가

--- jwtauth/serializers.py
+++ jwtauth/serializers.py
@@ -2,16 +2,28 @@ from rest_framework import serializers
 from .models import BlacklistedToken
 
 
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.ModelSerializer):
+    """
+    이 시리얼라이저는 로그인 요청을 처리합니다.
+    """
+
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
 
-class LogoutSerializer(serializers.Serializer):
+class LogoutSerializer(serializers.ModelSerializer):
+    """
+    이 시리얼라이저는 로그아웃 요청을 처리합니다.
+    """
+
     refresh_token = serializers.CharField()
 
 
-class RefreshTokenSerializer(serializers.Serializer):
+class RefreshTokenSerializer(serializers.ModelSerializer):
+    """
+    이 시리얼라이저는 리프레시 토큰을 검증합니다.
+    """
+
     refresh_token = serializers.CharField()
 
     def validate_refresh_token(self, value):

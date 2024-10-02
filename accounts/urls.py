5654e3ae137e7d6224abb75e1fb06ace6c14fbb5
ALbertIM0427 <guang7226961@naver.com>
2024-10-02 10:45:49 +0900

    fix: urls.py 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("users/", views.user_list_create, name="user-list-create"),
-    path("users/<int:pk>/", views.user_retrieve_update_delete, name="user-detail"),
-    path("users/counts/", views.user_count, name="user-count"),
-    path("managers/", views.manager_list_create, name="manager-list-create"),
+    path("users/", views.student_list_create, name="user-list-create"),
+    path("users/<int:pk>/", views.student_retrieve_update_delete, name="user-detail"),
+    path("users/counts/", views.student_count, name="user-count"),
+    path("managers/", views.tutor_list_create, name="manager-list-create"),
 ]

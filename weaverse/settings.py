2a144ad3a85aea2f6db3fb5956d379e2997ad50c
Nathan.LIU <nathanlyj@gmail.com>
2024-10-04 16:08:29 +0900

    134 인증 테스트 오류settings 파일 수정 (#135)
    * refactor:  API 클라이언트 수정, default_format을 "json"으로 설정하도록 변경함
    
    * refactor: REST_FRAMEWORK 오타 수정

--- weaverse/settings.py
+++ weaverse/settings.py
@@ -58,12 +58,16 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    "DEFAULT_RENDERER_CLASSES": (
+    "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",  # 이 옵션이 있어야 브라우저에서 API를 시각적으로 볼 수 있음
-    ),
-    "DEFAULT_AUTHENTICATION_CLASSES": ("jwtauth.authentication.JWTAuthentication",),
-    "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "jwtauth.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,

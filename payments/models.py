da3eca6271f9873adcbd4646babf95f0c9381a46
Yoo117 <zerial117@gmail.com>
2024-10-05 00:12:29 +0900

    refactor: order 및 cart 관련 모델 수정

--- payments/models.py
+++ payments/models.py
@@ -6,7 +6,7 @@ from courses.models import Curriculum, Course
 
 class Order(models.Model):
     """
-    주문 모델입니다. 주문의 상태를 포함하고 있습니다.
+    주문 모델입니다.
     """
 
     ORDER_STATUS_CHOICES = [
@@ -15,7 +15,7 @@ class Order(models.Model):
         ("canceled", "취소됨"),
     ]
 
-    user = models.OneToOneField(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="사용자"
     )
     order_status = models.CharField(
@@ -27,9 +27,18 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
 
+    @property
+    def total_items(self):
+        return sum(item.quantity for item in self.order_items.all())
+
+    @property
+    def total_price(self):
+        return sum(item.price for item in self.order_items.all())
+
     class Meta:
         ordering = ["-created_at"]
         verbose_name = "주문"
+        verbose_name_plural = "주문들"
 
     def __str__(self):
         return f"{self.user.nickname}의 주문"
@@ -57,17 +66,29 @@ class OrderItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
 
+    @property
+    def item_name(self):
+        return (
+            self.curriculum.name
+            if self.curriculum
+            else self.course.title if self.course else "상품"
+        )
+
+    @property
+    def price(self):
+        return (
+            self.curriculum.price
+            if self.curriculum
+            else self.course.price if self.course else 0
+        )
+
     class Meta:
         ordering = ["-created_at"]
         verbose_name = "주문 상품"
         verbose_name_plural = "주문 상품들"
 
     def __str__(self):
-        if self.item_type == "curriculum":
-            return f"{self.user.nickname}의 주문에 포함된 {self.curriculum.name}"
-        elif self.item_type == "course":
-            return f"{self.user.nickname}의 주문에 포함된 {self.course.title}"
-        return ValueError("상품 종류가 올바르지 않습니다.")
+        return f"{self.cart.user.nickname}의 주문에 있는 {self.item_name}"
 
 
 class Cart(models.Model):
@@ -80,6 +101,14 @@ class Cart(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
+
+    @property
+    def total_items(self):
+        return sum(item.quantity for item in self.cart_items.all())
+
+    @property
+    def total_price(self):
+        return sum(item.price for item in self.cart_items.all())
 
     class Meta:
         verbose_name = "장바구니"
@@ -113,64 +142,73 @@ class CartItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
 
+    @property
+    def item_name(self):
+        return (
+            self.curriculum.name
+            if self.curriculum
+            else self.course.title if self.course else "상품"
+        )
+
+    @property
+    def price(self):
+        return (
+            self.curriculum.price
+            if self.curriculum
+            else self.course.price if self.course else 0
+        )
+
     class Meta:
         ordering = ["-created_at"]
         verbose_name = "장바구니 상품"
         verbose_name_plural = "장바구니 상품들"
 
     def __str__(self):
-        if self.item_type == "curriculum":
-            return f"{self.user.nickname}의 장바구니에 담긴 {self.curriculum.name}"
-        elif self.item_type == "course":
-            return f"{self.user.nickname}의 장바구니에 담긴 {self.course.title}"
-        return ValueError("상품 종류가 올바르지 않습니다.")
+        return f"{self.cart.user.nickname}의 장바구니에 있는 {self.item_name}"
 
 
-class Coupon(models.Model):
+class Payment(models.Model):
     """
-    쿠폰 모델입니다.
+    결제 모델입니다.
     """
 
-    CODE_TYPE_CHOICES = [
-        ("fixed", "고정 금액 할인"),
-        ("percentage", "정률 할인"),
+    PAYMENT_STATUS_CHOICES = [
+        ("pending", "대기 중"),
+        ("completed", "완료됨"),
+        ("canceled", "취소됨"),
     ]
 
-    APPLICABLE_TYPE_CHOICES = (
-        ("curriculum", "커리큘럼"),
-        ("course", "코스"),
-    )
+    PAYMENT_METHOD_CHOICES = [
+        ("credit_card", "신용카드"),
+        ("kakaopay", "카카오페이"),
+        ("bank_transfer", "계좌 이체"),
+    ]
 
-    code = models.CharField(max_length=50, unique=True, verbose_name="코드")
-    discount_type = models.CharField(
-        max_length=10, choices=CODE_TYPE_CHOICES, verbose_name="할인 유형"
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="사용자"
     )
-    discount_value = models.PositiveIntegerField(verbose_name="할인 가치")
-    expiration_date = models.DateTimeField(verbose_name="만료일")
-    curriculum = models.OneToOneField(
-        Curriculum,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        verbose_name="커리큘럼",
-    )
-    course = models.OneToOneField(
-        Course, on_delete=models.CASCADE, null=True, blank=True, verbose_name="코스"
-    )
-    applicable_type = models.CharField(
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, verbose_name="주문")
+    payment_status = models.CharField(
         max_length=10,
-        choices=APPLICABLE_TYPE_CHOICES,
-        default="curriculum",
-        verbose_name="적용 유형",
+        choices=PAYMENT_STATUS_CHOICES,
+        default="pending",
+        verbose_name="결제 상태",
     )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHOD_CHOICES,
+        default="credit_card",
+        verbose_name="결제 방식",
+    )
+    payment_amount = models.PositiveIntegerField(verbose_name="결제 금액")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
 
     class Meta:
-        verbose_name = "쿠폰"
+        verbose_name = "결제"
 
     def __str__(self):
-        return f"쿠폰 {self.id} - 생성일: {self.created_at.strftime('%Y-%m-%d')}"
+        return f"{self.user.nickname}의 결제"
 
 
 class UserBillingAddress(models.Model):
@@ -258,27 +296,3 @@ class UserBillingInfo(models.Model):
 
     def __str__(self):
         return f"{self.user.nickname}의 청구 정보"
-
-
-class UserPurchaseHistory(models.Model):
-    """
-    사용자의 구매 내역 모델입니다.
-    """
-
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="사용자"
-    )
-    order = models.OneToOneField(
-        Order,
-        on_delete=models.CASCADE,
-        verbose_name="주문",
-        related_name="purchase_history",
-    )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
-
-    class Meta:
-        verbose_name = "사용자 구매 내역"
-
-    def __str__(self):
-        return f"{self.user.nickname}의 구매 내역"

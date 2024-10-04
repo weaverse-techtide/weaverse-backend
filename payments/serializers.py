e3df940b31d8a7749513f5333932111e2b21c8a4
Yoo117 <zerial117@gmail.com>
2024-10-05 00:15:16 +0900

    refactor: cart 및 order 관련 serializer 수정

--- payments/serializers.py
+++ payments/serializers.py
@@ -3,109 +3,10 @@ from rest_framework import serializers
 from .models import Cart, CartItem, Order, OrderItem
 
 
-class CartItemSerializer(serializers.ModelSerializer):
-    """
-    장바구니에 담긴 상품 모델의 시리얼라이저입니다.
-    """
-
-    curriculum_name = serializers.SerializerMethodField()
-    course_title = serializers.SerializerMethodField()
-    curriculum_price = serializers.SerializerMethodField()
-    course_price = serializers.SerializerMethodField()
-
-    class Meta:
-        model = CartItem
-        fields = [
-            "id",
-            "cart",
-            "curriculum",
-            "course",
-            "quantity",
-            "created_at",
-            "updated_at",
-            "curriculum_name",
-            "course_title",
-            "curriculum_price",
-            "course_price",
-        ]
-        read_only_fields = [
-            "curriculum_name",
-            "course_title",
-            "curriculum_price",
-            "course_price",
-            "quantity",
-        ]
-
-    def get_curriculum_name(self, obj):
-        return obj.curriculum.name if obj.curriculum else None
-
-    def get_course_title(self, obj):
-        return obj.course.title if obj.course else None
-
-    def get_curriculum_price(self, obj):
-        return obj.curriculum.price if obj.curriculum else None
-
-    def get_course_price(self, obj):
-        return obj.course.price if obj.course else None
-
-    def validate(self, data):
-        # 커리큘럼과 코스 중 하나만 선택되었는지 확인합니다.
-        curriculum = data.get("curriculum")
-        course = data.get("course")
-        if not curriculum and not course:
-            raise serializers.ValidationError(
-                "커리큘럼 또는 코스 중 하나를 선택해야 합니다."
-            )
-        if curriculum and course:
-            raise serializers.ValidationError(
-                "커리큘럼과 코스 중 하나만 선택해야 합니다."
-            )
-        return data
-
-
-class CartSerializer(serializers.ModelSerializer):
-    """
-    장바구니 모델의 시리얼라이저입니다. 총 상품 수량과 가격을 계산합니다.
-    """
-
-    cart_items = CartItemSerializer(many=True, read_only=True)
-    total_items = serializers.SerializerMethodField()
-    total_price = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Order
-        fields = [
-            "id",
-            "user",
-            "cart_items",
-            "total_items",
-            "total_price",
-            "created_at",
-            "updated_at",
-        ]
-
-    def get_total_items(self, obj):
-        return obj.cart_items.count()
-
-    def get_total_price(self, obj):
-        total = sum(
-            [
-                item.curriculum.price if item.curriculum else item.course.price
-                for item in obj.cart_items.all()
-            ]
-        )
-        return total
-
-
 class OrderItemSerializer(serializers.ModelSerializer):
     """
     주문 상품 모델의 시리얼라이저입니다.
     """
-
-    curriculum_name = serializers.SerializerMethodField()
-    course_title = serializers.SerializerMethodField()
-    curriculum_price = serializers.SerializerMethodField()
-    course_price = serializers.SerializerMethodField()
 
     class Meta:
         model = OrderItem
@@ -116,30 +17,14 @@ class OrderItemSerializer(serializers.ModelSerializer):
             "quantity",
             "created_at",
             "updated_at",
-            "curriculum_name",
-            "course_title",
-            "curriculum_price",
-            "course_price",
+            "item_name",
+            "price",
         ]
         read_only_fields = [
-            "curriculum_name",
-            "course_title",
-            "curriculum_price",
-            "course_price",
+            "item_name",
+            "price",
             "quantity",
         ]
-
-    def get_curriculum_name(self, obj):
-        return obj.curriculum.name if obj.curriculum else None
-
-    def get_course_title(self, obj):
-        return obj.course.title if obj.course else None
-
-    def get_curriculum_price(self, obj):
-        return obj.curriculum.price if obj.curriculum else None
-
-    def get_course_price(self, obj):
-        return obj.course.price if obj.course else None
 
     def validate(self, data):
         # 커리큘럼과 코스 중 하나만 선택되었는지 확인합니다.
@@ -162,11 +47,9 @@ class OrderSerializer(serializers.ModelSerializer):
     """
 
     order_items = OrderItemSerializer(many=True, read_only=True)
-    total_items = serializers.SerializerMethodField()
-    total_price = serializers.SerializerMethodField()
 
     class Meta:
-        model = Cart
+        model = Order
         fields = [
             "id",
             "user",
@@ -176,15 +59,70 @@ class OrderSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
+        read_only_fields = [
+            "total_items",
+            "total_price",
+        ]
 
-    def get_total_items(self, obj):
-        return obj.order_items.count()
 
-    def get_total_price(self, obj):
-        total = sum(
-            [
-                item.curriculum.price if item.curriculum else item.course.price
-                for item in obj.order_items.all()
-            ]
-        )
-        return total
+class CartItemSerializer(serializers.ModelSerializer):
+    """
+    장바구니에 담긴 상품 모델의 시리얼라이저입니다.
+    """
+
+    class Meta:
+        model = CartItem
+        fields = [
+            "id",
+            "cart",
+            "curriculum",
+            "course",
+            "quantity",
+            "created_at",
+            "updated_at",
+            "item_name",
+            "price",
+        ]
+        read_only_fields = [
+            "item_name",
+            "price",
+            "quantity",
+        ]
+
+    def validate(self, data):
+        # 커리큘럼과 코스 중 하나만 선택되었는지 확인합니다.
+        curriculum = data.get("curriculum")
+        course = data.get("course")
+        if not curriculum and not course:
+            raise serializers.ValidationError(
+                "커리큘럼 또는 코스 중 하나를 선택해야 합니다."
+            )
+        if curriculum and course:
+            raise serializers.ValidationError(
+                "커리큘럼과 코스 중 하나만 선택해야 합니다."
+            )
+        return data
+
+
+class CartSerializer(serializers.ModelSerializer):
+    """
+    장바구니 모델의 시리얼라이저입니다. 총 상품 수량과 가격을 계산합니다.
+    """
+
+    cart_items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = [
+            "id",
+            "user",
+            "cart_items",
+            "total_items",
+            "total_price",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "total_items",
+            "total_price",
+        ]

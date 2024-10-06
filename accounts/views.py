1b7d27fd3149b8cfc288631fe792ee8527ad22dc
yujeong <devnproyj22@gmail.com>
2024-10-06 15:38:41 +0900

    refactor: accounts/views.py 수정 - Not Found 임포트 삭제

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.db.utils import DatabaseError
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, generics, mixins, status
-from rest_framework.exceptions import NotFound, PermissionDenied, ValidationError
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 

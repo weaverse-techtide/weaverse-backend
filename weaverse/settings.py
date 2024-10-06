2bf54cf118c85010c4c3266c0e5d262a112c8f69
yujeong <devnproyj22@gmail.com>
2024-10-06 16:09:13 +0900

    refactor: weaverse/settings.py 수정
    - AWS S3 스토리지 설정 추가

--- weaverse/settings.py
+++ weaverse/settings.py
@@ -135,3 +135,15 @@ SPECTACULAR_SETTINGS = {
     "SERVE_URLCONF": "weaverse.urls",
     "EXTERNAL_DOCS": {"description": "Weaverse GitHub", "url": ""},
 }
+
+# S3 설정
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"

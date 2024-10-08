2864f432b8a8265c1c619e38f3557f3ca12fe850
nathanLYJ <nathanlyj@gmail.com>
2024-10-08 12:59:31 +0900

    refactor: 소셜로그인 urls 추가

--- jwtauth/urls.py
+++ jwtauth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, LogoutView, RefreshTokenView
+from .views import LoginView, LogoutView, RefreshTokenView, SocialLoginView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("refresh/", RefreshTokenView.as_view(), name="refresh"),
+    path("social-login/", SocialLoginView.as_view(), name="social_login"),
 ]

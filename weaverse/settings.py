b3e0cc0a7af642f692a3f2c2f56db1c2c916caba
ALbertIM0427 <guang7226961@naver.com>
2024-10-08 17:55:31 +0900

    test: 테스트 오류 수정

--- weaverse/settings.py
+++ weaverse/settings.py
@@ -70,9 +70,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "jwtauth.authentication.JWTAuthentication",
     ],
-    "DEFAULT_PARSER_CLASSES": [
-        "rest_framework.parsers.JSONParser",
-    ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,

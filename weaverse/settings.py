83fe8a1bc13b04d76a6f19ee7ea737602bcd9e0d
yujeong <devnproyj22@gmail.com>
2024-10-10 14:28:25 +0900

    refactor: weaverse/settings.py 수정 - CORS, AWS S3, MEDIA_URL

--- weaverse/settings.py
+++ weaverse/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "weaverse.urls"

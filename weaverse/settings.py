e8b98728e80aab3f5a09fce168b5a3dc5178426f
yujeong <devnproyj22@gmail.com>
2024-10-10 11:28:32 +0900

    refactor: weaverse/settings.py 수정 - S3 연동

--- weaverse/settings.py
+++ weaverse/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "weaverse.urls"

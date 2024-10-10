fd627bdba109c3ccb91d75dbb415488b106648ec
nathanLYJ <nathanlyj@gmail.com>
2024-10-10 14:55:10 +0900

    refactor: Add GoogleLogin view to jwtauth/urls.py

--- jwtauth/urls.py
+++ jwtauth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoginView, LogoutView, RefreshTokenView
+from .views import LoginView, LogoutView, RefreshTokenView, GoogleLogin
+
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("refresh/", RefreshTokenView.as_view(), name="refresh"),
+    path("social-login/google/", GoogleLogin.as_view(), name="google_login"),
 ]

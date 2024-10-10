612bb4184ef2e030d0d808a0fbaa5c2dd584177b
yujeong <devnproyj22@gmail.com>
2024-10-10 10:08:57 +0900

    refactor: materials/serializers.py  - 시리얼라이저 추가

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,7 +2,7 @@ from accounts.models import CustomUser
 from courses.models import Course, Topic
 from rest_framework import serializers
 
-from .models import Image, Video
+from .models import Image, Video, VideoEventData
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -57,6 +57,67 @@ class VideoSerializer(serializers.ModelSerializer):
                     "이 topic에 영상을 넣을 권한이 없습니다."
                 )
         return value
+
+
+class VideoEventSerializer(serializers.ModelSerializer):
+
+    video_url = serializers.URLField(
+        write_only=True
+    )  # 클라이언트가 보내는 video_url을 받기 위한 필드
+
+    class Meta:
+        model = VideoEventData
+        fields = ["id", "video_url", "duration", "current_time", "event_type"]
+        read_only_fields = ["id", "timestamp"]
+
+    def validate_duration(self, value):
+        if value < 0:
+            raise serializers.ValidationError("올바른 영상 재생시간이 아닙니다.")
+        return value
+
+    def validate_current_time(self, value):  # 필드 이름 수정
+        if value < 0:
+            raise serializers.ValidationError("올바른 영상 현재 재생시간이 아닙니다.")
+        return value
+
+    def validate_event_type(self, value):
+        """
+        event_type 값이 선택된 EVENT_CHOICES 중 하나인지 확인합니다.
+        """
+        valid_choices = dict(VideoEventData.EVENT_CHOICES).keys()
+        if value not in valid_choices:
+            raise serializers.ValidationError(f"{value}는 유효한 이벤트가 아닙니다.")
+        return value
+
+    def validate(self, data):
+        if data["duration"] < data["current_time"]:
+            raise serializers.ValidationError("올바른 영상 재생시간 관계가 아닙니다.")
+        return data
+
+    def create(self, validated_data):
+        """
+        VideoEventData를 생성합니다.
+        - VideoEventData를 받기 전에, 먼저 S3에 존재하는 Video 인스턴스인지 확인하고
+        - 그러하다면 해당 Video 인스턴스와 관계된 VideoEventData를 생성합니다.
+
+        """
+        video_url = validated_data.pop("video_url", None)
+
+        try:
+            video_instance = Video.objects.get(file=video_url)
+        except Video.DoesNotExist:
+            raise serializers.ValidationError(
+                "해당 URL과 일치하는 영상 파일이 없습니다."
+            )
+        video_event_data = VideoEventData.objects.create(
+            video=video_instance, **validated_data
+        )
+
+        return video_event_data
+
+
+class ViewEventListSerializer(serializers.ModelSerializer):
+    pass
 
 
 class WatchHistorySerializer(serializers.Serializer):

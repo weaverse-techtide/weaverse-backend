a9139dce8eeb29f3967789309cc6d5dd02d94f0c
yujeong <devnproyj22@gmail.com>
2024-10-14 14:19:57 +0900

    refactor: materials/urls.py 수정 - id 추가

--- materials/urls.py
+++ materials/urls.py
@@ -7,29 +7,34 @@ app_name = "materials"
 urlpatterns = [
     # 이미지 관련 URL
     path("images/upload/", views.ImageCreateView.as_view(), name="image-upload"),
-    path("images/", views.ImageListCreateView.as_view(), name="image-list-create"),
+    path("images/", views.ImageListView.as_view(), name="image-list-create"),
     path(
         "images/<int:pk>/",
         views.ImageRetrieveUpdateDestroyView.as_view(),
         name="image-detail",
     ),
-    # 비디오 관련 URL
+    # 동영상 관련 URL
     path("videos/upload/", views.VideoCreateView.as_view(), name="video-upload"),
-    path("videos/", views.VideoListCreateView.as_view(), name="video-list"),
+    path("videos/", views.VideoListView.as_view(), name="video-list"),
     path(
         "videos/<int:pk>/",
         views.VideoRetrieveUpdateDestroyView.as_view(),
         name="video-detail",
     ),
-    # 사용자 비디오 시청 기록 관련 URL
+    # 사용자 동영상 이벤트 관련 URL
     path(
-        "video-event-data/",
-        views.VideoEventCreateView.as_view(),
+        "user/<int:user_id>/video/<int:video_id>/event-occur/",
+        views.UserVideoEventCreateView.as_view(),
         name="video-event-data",
     ),
     path(
-        "users/<int:user_id>/videos/<int:video_id>/watch-history/",
-        views.UserVideoEventListView.as_view(),
+        "user/<int:user_id>/video/<int:video_id>/history/",
+        views.UserVideoEventListView(),
         name="video-event-list",
+    ),
+    path(
+        "user/<int:user_id>/video/<int:video_id>/history/<int:event_id>",
+        views.UserVideoEventRetrieveUpdateDestroyView(),
+        name="video-event-detail",
     ),
 ]
